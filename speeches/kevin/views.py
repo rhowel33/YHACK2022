56bbab022c6b41544052c3ec473fb4cc46e00afa
Elijah <elicox0@gmail.com>
2022-03-26 14:13:10 -0600

    restructure

--- speeches/kevin/views.py
+++ speeches/kevin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing(request):
-    with open("../test_speech.txt") as fin:
+    with open("../generated/test_speech.txt") as fin:
         text = fin.read()
     return HttpResponse(text)
 
